6625ec60df36ad24f4782c316db7d2af2e41201f
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2023-09-27 12:36:56 +0700

    Jawaban pemilihan percobaan 2

--- PemilihanPercobaan213Huruf.java
+++ PemilihanPercobaan213Huruf.java
@@ -14,22 +14,22 @@ public class PemilihanPercobaan213Huruf {
         System.out.println("Nilai tugas : ");
         float tugas = input13.nextFloat();
 
-        float total = ((uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas + 0.2f));
+        float total = ((uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f));
         
         if (total > 80) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai huruf anda A");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai huruf anda A");}
         else if (total > 73) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda B+");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda B+");}
         else if (total > 65) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda B");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda B");}
         else if (total > 60) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda C+");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda C+");}
         else if (total > 50) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda C");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda C");}
         else if (total > 39) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda D");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda D");}
         else if (total <= 39) {
-            System.out.println("Nilai IPK anda adalah : " + total + "Nilai anda E");}
+            System.out.println("Nilai IPK anda adalah : " + total + ". Nilai anda E");}
         }
     }
 
