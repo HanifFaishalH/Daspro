baebc1551c81fdf666b19863d8264c6012b31650
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2023-10-03 16:33:47 +0700

    Modifikasi program kode tahun kabisat

--- Pemilihan2Percobaan113pertanyaan.java
+++ Pemilihan2Percobaan113pertanyaan.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan113 {
+public class Pemilihan2Percobaan113pertanyaan {
     public static void main(String[] args) {
-        
         Scanner input13 = new Scanner(System.in);
 
         System.out.println("Masukkan tahun : ");
         int tahun = input13.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+        if ((tahun % 4) == 0 && (tahun % 100) != 0 || tahun % 400 == 0) {
             System.out.println(tahun + " adalah tahun kabisat");
         } else {
             System.out.println(tahun + " bukan tahun kabisat");
