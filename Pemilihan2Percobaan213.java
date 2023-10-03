9c72d02eaa72a2c79953b033661d896da80c535b
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2023-10-03 14:42:39 +0700

    Pemilihan 2 Percobaan 2 absen 13

--- Pemilihan2Percobaan213.java
+++ Pemilihan2Percobaan213.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Pemilihan2Percobaan213 {
-    
+    public static void main(String[] args) {
+        
+        Scanner input13 = new Scanner(System.in);
+
+        Float totalsudut;
+
+        System.out.println("Masukkan besar sudut 1 : ");
+        Float sudut1 = input13.nextFloat();
+        System.out.println("Masukkan besar sudut 2 : ");
+        Float sudut2 = input13.nextFloat();
+        System.out.println("Masukkan besar sudut 3 : ");
+        Float sudut3 = input13.nextFloat();
+
+        totalsudut = sudut1 + sudut2 + sudut3;
+
+        if (totalsudut == 180) {
+            if (sudut1 == 90 || sudut2 == 90 || sudut3 == 90)
+                System.out.println("Segitiga siku-siku");
+            else 
+                System.out.println("Bukan segitiga siku-siku");
+        } else {
+            System.out.println("Bukan segitiga");
+        }
+
+    }
 }
