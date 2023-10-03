271853b29dd16ac6269142a94f9e43fd046f5e2f
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2023-10-03 17:09:35 +0700

    Kode program jenis segitiga berdasarkan sudut

--- Pemilihan2Percobaan113pertanyaan.java
+++ Pemilihan2Percobaan113pertanyaan.java
@@ -7,7 +7,7 @@ public class Pemilihan2Percobaan113pertanyaan {
         System.out.println("Masukkan tahun : ");
         int tahun = input13.nextInt();
 
-        if ((tahun % 4) == 0 && (tahun % 100) != 0 || tahun % 400 == 0) {
+        if ((tahun % 4) == 0 && (tahun % 100) != 0 || tahun % 400 == 0 ) {
             System.out.println(tahun + " adalah tahun kabisat");
         } else {
             System.out.println(tahun + " bukan tahun kabisat");
---
+++ Pemilihan2Percobaan213modifikasi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan213modifikasi {
+    public static void main(String[] args) {
+        
+        Scanner input13 = new Scanner(System.in);
+
+        Float totalsudut;
+
+        System.out.println("Masukkan besar sudut 1 : ");
+        Float sudut1 = input13.nextFloat();
+        System.out.println("Masukkan besar sudut 2 : ");
+        Float sudut2 = input13.nextFloat();
+        System.out.println("Masukkan besar sudut 3 : ");
+        Float sudut3 = input13.nextFloat();
+
+        totalsudut = sudut1 + sudut2 + sudut3;
+
+        if (totalsudut == 180) {
+            if (sudut1 == 90 || sudut2 == 90 || sudut3 == 90)
+                System.out.println("Segitiga siku-siku");
+            else if (sudut1 >= 90 || sudut2 >= 90 || sudut3 >= 90)
+                System.out.println("Segitiga tumpul");
+        } else {
+            System.out.println("Segitiga lancip");
+        }
+    }
+}
