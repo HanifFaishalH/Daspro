2a06adb29be94fa87446c702cf7c9394e2be47ee
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2023-10-04 12:22:23 +0700

    Jobsheet 6 tugas

---
+++ latihanMandiri1.java
@@ -0,0 +1,16 @@
+public class latihanMandiri1 {
+    public static void main(String[] args) {
+        
+        int bil1 = 28, bil2 = 54, bil3 = 15;
+
+        if (bil2 > bil1) {
+            if (bil2 > bil3) {
+                System.out.println(bil2 + " adalah bilangan terbesar");
+            } else {
+                System.out.println("Error");
+            }
+        } else {
+            System.out.println("error");
+        }
+    }
+}
---
+++ latihanMandiri2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;;
+
+public class latihanMandiri2 {
+    public static void main(String[] args) {
+        
+        String buku, kamus, novel;
+        int jumlahBuku;
+        float Diskon;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan jenis buku : ");
+        buku = input.nextLine();
+        System.out.println("Masukkan jumlah buku : ");
+        jumlahBuku = input.nextInt();
+
+        if (buku.equalsIgnoreCase("kamus")) {
+            if (jumlahBuku > 2)
+                System.out.println("Dapat diskon 10% + 2%");
+            else
+                System.out.println("Dapat diskon 10%");    
+        }
+        else if (buku.equalsIgnoreCase("novel"))
+            if (jumlahBuku > 3) {
+                System.out.println("Dapat diskon 7% + 2%");
+            } else {
+                System.out.println("Dapat diskon 7% + 1%");
+            }
+        else {
+            if (jumlahBuku > 3) {
+                System.out.println("Dapat diskon 5%");
+            } else {
+                System.out.println("Nggak dapat diskon hiya");
+            }
+        }
+    }
+}
